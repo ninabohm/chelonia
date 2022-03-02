7c5516a86f8f4aca7f6fb86a4b73f3feb264e514
ninabohm <nina.boehm1994@gmail.com>
2022-03-02 17:05:49 +0100

    initial pyautogui setup

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-import time
-
-import redis
+import pyautogui
 from flask import Flask
 
-app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+
+# app = Flask(__name__)
 
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+def initialize_clicking():
+    screen_width, screen_height = pyautogui.size()
+    current_mouse_x = pyautogui.position()
+    current_mouse_y = pyautogui.position()
+    print(screen_width, screen_height, current_mouse_x, current_mouse_y)
+    pyautogui.moveTo(100, 150)
+    pyautogui.click()
+    pyautogui.click(100, 200)
+    pyautogui.write("hello world")
 
 
-@app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+# @app.route('/')
+# def hello():
+#     return 'Hello World :)'
+
+
+if __name__ == '__main__':
+    initialize_clicking()
