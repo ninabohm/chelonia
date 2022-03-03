1155d022c15d640d99c7c4f68b3a2a01fc7ba546
ninabohm <nina.boehm1994@gmail.com>
2022-03-03 12:16:49 +0100

    complete checkout, yesss

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import time
 from flask import Flask
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
 # app = Flask(__name__)
 driver = webdriver.Chrome('./chromedriver')
@@ -8,7 +11,7 @@ driver = webdriver.Chrome('./chromedriver')
 
 def open_booking():
     url = "https://pretix.eu/Baeder/74/"
-    datetime_selector = ".event-time[data-time='2022-03-04T14:30:00+00:00']"
+    datetime_selector = ".event-time[data-time='2022-03-04T19:00:00+00:00']"
     driver.get(url)
     booking_date = driver.find_element(By.CSS_SELECTOR, datetime_selector)
     booking_date.click()
@@ -22,9 +25,49 @@ def open_booking():
 
     add_to_cart_button = driver.find_element(By.ID, "btn-add-to-cart")
     add_to_cart_button.click()
+    time.sleep(3)
 
-    checkout_button = driver.find_element(By.CSS_SELECTOR, "button[class='btn btn-block btn-primary']")
-    checkout_button.click()
+    complete_checkout()
+
+
+def complete_checkout():
+    try:
+        checkout_url = "https://pretix.eu/Baeder/74/checkout/customer/"
+        driver.get(checkout_url)
+
+        login_radio = driver.find_element(By.ID, "input_customer_login")
+        login_radio.click()
+        time.sleep(3)
+
+        login()
+
+        confirmation_checkbox = driver.find_element(By.ID, "input_confirm_confirm_text_0")
+        confirmation_checkbox.click()
+        confirmation_checkbox.send_keys(Keys.TAB)
+
+    except NoSuchElementException as error:
+        print(error)
+
+
+def login():
+    user_email = driver.find_element(By.ID, "id_login-email")
+    user_email.send_keys("nina.boehm1994@gmail.com")
+    user_password = driver.find_element(By.ID, "id_login-password")
+    user_password.send_keys("Pgvx/pF4;87$")
+    user_password.send_keys(Keys.TAB)
+    user_password.send_keys(Keys.TAB)
+    user_password.send_keys(Keys.TAB)
+    user_password.send_keys(Keys.ENTER)
+    time.sleep(3)
+
+    checkbox_save = driver.find_element(By.CSS_SELECTOR, "input[type='checkbox']")
+    checkbox_save.click()
+    checkbox_save.send_keys(Keys.TAB)
+    checkbox_save.send_keys(Keys.TAB)
+    checkbox_save.send_keys(Keys.TAB)
+    checkbox_save.send_keys(Keys.ENTER)
+    time.sleep(3)
+
 
 # @app.route('/')
 # def hello():
