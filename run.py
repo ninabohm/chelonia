9297277a3027bf69015c3fc914a023561904f0bc
ninabohm <nina.boehm1994@gmail.com>
2022-03-11 12:05:06 +0100

    remove pysomething from requirements

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import os
 from app import app
 
 if __name__ == '__main__':
-    port = os.environ.get('PORT')
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0')
+
