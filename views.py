03ccb4896ba7b0601c154f02eae10d70e7c113c4
ninabohm <nina.boehm1994@gmail.com>
2022-03-12 20:09:48 +0100

    argument

--- views.py
+++ views.py
@@ -332,9 +332,9 @@ def start_ticket(booking_id, current_user_id):
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_arguments("--headless")
-    chrome_options.add_arguments("--disable-dev-shm-usage")
-    chrome_options.add_arguments("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
     app.logger.info(f"initialized chrome driver")
 
