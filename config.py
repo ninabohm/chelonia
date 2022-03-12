7bfe909fe7da7acd2b79469b8847f4b1bc85dddf
ninabohm <nina.boehm1994@gmail.com>
2022-03-12 17:31:45 +0100

    update config

--- config.py
+++ config.py
@@ -9,18 +9,19 @@ class Config(object):
     SESSION_REDIS = redis.from_url(os.environ.get('SESSION_REDIS'))
     SESSION_TYPE = os.environ.get('SESSION_TYPE')
     FLASK_ENV = os.environ.get('FLASK_ENV')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):
     DEBUG = False
-
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    CELERY_RESULT_BACKEND = os.environ['CELERY_RESULT_BACKEND']
 
 class DevelopmentConfig(Config):
     DEBUG = True
     DEVELOPMENT = True
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     CELERY_RESULT_BACKEND = os.environ['CELERY_RESULT_BACKEND']
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestingConfig(Config):
