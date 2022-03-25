c47e73a7bf155f27a01eb0bef7f4daaa4e035ed0
ninabohm <nina.boehm1994@gmail.com>
2022-03-25 10:25:05 +0200

    json logs test

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 import logging
+import os
+
+import json_log_formatter
 from flask import Flask, g, render_template, request, redirect, flash, url_for, jsonify, session
 from flask_login import LoginManager, login_user, current_user, logout_user
 from flask_celery import make_celery
 from flask_session import Session
 
+formatter = json_log_formatter.JSONFormatter()
+json_handler = logging.FileHandler(filename='/var/log/chelonia-log.json')
+json_handler.setFormatter(formatter)
+
+logger = logging.getLogger('chelonia-log')
+logger.addHandler(json_handler)
+logger.setLevel(logging.INFO)
 
 app = Flask(__name__)
 if app.config["ENV"] == "production":
     app.config.from_object("config.ProductionConfig")
 else:
     app.config.from_object("config.DevelopmentConfig")
-app.logger.setLevel(logging.INFO)
 app.config['TIMEZONE'] = "UTC"
 sess = Session()
 
@@ -21,5 +30,8 @@ login_manager.init_app(app)
 login_manager.login_view = "login"
 
 sess.init_app(app)
+
+
+
 
 import views
