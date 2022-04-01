ededab71f1b37f25da3e6d0c55662352432e0c22
ninabohm <nina.boehm1994@gmail.com>
2022-04-01 07:31:46 +0200

    force rebuild

--- views.py
+++ views.py
@@ -331,7 +331,6 @@ def start_ticket(booking_id, current_user_id):
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    # TODO: save headless as env var for local debugging
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
